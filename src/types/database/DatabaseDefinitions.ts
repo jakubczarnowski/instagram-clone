9ad3738332fd28d22edf05f5354f1dfac47938e0
jakubczarnowski <rowerkuby123@gmail.com>
2023-05-04 00:34:14 +0200

    fix database

--- src/types/database/DatabaseDefinitions.ts
+++ src/types/database/DatabaseDefinitions.ts
@@ -9,7 +9,109 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
-      [_ in never]: never
+      comment_likes: {
+        Row: {
+          created_at: string
+          post_id: number
+          user_id: string
+        }
+        Insert: {
+          created_at?: string
+          post_id: number
+          user_id: string
+        }
+        Update: {
+          created_at?: string
+          post_id?: number
+          user_id?: string
+        }
+      }
+      comments: {
+        Row: {
+          content: string
+          created_at: string
+          id: number
+          post_id: number
+          user_id: string
+        }
+        Insert: {
+          content: string
+          created_at?: string
+          id?: number
+          post_id: number
+          user_id: string
+        }
+        Update: {
+          content?: string
+          created_at?: string
+          id?: number
+          post_id?: number
+          user_id?: string
+        }
+      }
+      post_likes: {
+        Row: {
+          created_at: string
+          post_id: number
+          user_id: string
+        }
+        Insert: {
+          created_at?: string
+          post_id: number
+          user_id: string
+        }
+        Update: {
+          created_at?: string
+          post_id?: number
+          user_id?: string
+        }
+      }
+      posts: {
+        Row: {
+          created_at: string
+          id: number
+          image_url: string
+          title: string
+          user_id: string
+        }
+        Insert: {
+          created_at?: string
+          id?: number
+          image_url: string
+          title: string
+          user_id: string
+        }
+        Update: {
+          created_at?: string
+          id?: number
+          image_url?: string
+          title?: string
+          user_id?: string
+        }
+      }
+      profiles: {
+        Row: {
+          avatar_url: string | null
+          bio: string | null
+          id: string
+          updated_at: string | null
+          username: string | null
+        }
+        Insert: {
+          avatar_url?: string | null
+          bio?: string | null
+          id: string
+          updated_at?: string | null
+          username?: string | null
+        }
+        Update: {
+          avatar_url?: string | null
+          bio?: string | null
+          id?: string
+          updated_at?: string | null
+          username?: string | null
+        }
+      }
     }
     Views: {
       [_ in never]: never
